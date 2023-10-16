76075ace7299e8dd97c0a5e89049fbc7bde12895
Armaiic <bastiendandre.pro@gmail.com>
2023-10-16 12:13:30 +0200

    corrected_2

--- Cli.java
+++ Cli.java
@@ -1,4 +1,6 @@
 import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.time.LocalDateTime;
 
 public class Cli {
@@ -7,52 +9,39 @@ public class Cli {
         Scanner scanner = new Scanner(System.in);
         System.out.print("> ");
         String command;
-		
+
         while (!(command = scanner.nextLine()).equals("exit")) {
             String output = "";
              command = command.trim();
 
             if (command.equals("date")) {
-                LocalDateTime now = LocalDateTime.now();
-                output = now.toLocalDate().toString(); // Get the unformatted date.
-            } 
-            else if (command.equals("time")) {
-                LocalDateTime now = LocalDateTime.now();
-                output = now.toLocalTime().toString(); // Get the unformatted time.
-            }  
-            else if (command.equals("datetime")) {
+                LocalDate now = LocalDate.now();
+                output = now.toString();
+            } else if (command.equals("time")) {
+                LocalTime now = LocalTime.now();
+                output = now.toString();
+            } else if (command.equals("datetime")) {
                 LocalDateTime now = LocalDateTime.now();
                 output = now.toString();
-            }
-            else if (command.equals("useraccount")) {
+            } else if (command.equals("useraccount")) {
                 output = System.getProperty("user.name");
-            } 
-            else if (command.equals("userhome")) {
+            } else if (command.equals("userhome")) {
                 output = System.getProperty("user.home");
-            } 
-            else if (command.equals("os")) {
+            } else if (command.equals("os")) {
                 output = System.getProperty("os.name") + " (" + System.getProperty("os.version") + ")";
-            }
-            else if (command.startsWith("printenv")) {
+            } else if (command.startsWith("printenv")) {
                 String varName = command.substring(8); // "printenv".length()
                 varName = varName.trim(); // Remove leading and trailing spaces
-                if (varName.isEmpty()) {
-                    output = "Missing arguments. Usage: printenv <variable_name>";
-                } else {
-                    String envValue = System.getenv(varName);
+                String envValue = System.getenv(varName);
                     if (envValue != null) {
                         output = envValue;
-                    } else {
-                        output = "Environment variable '" + varName + "' not found.";
-                    }
+                    } 
                 }
-            } 
-            else if (command.startsWith("echo")) {
+             else if (command.startsWith("echo")) {
                 String arguments = command.substring(4); // "echo".length()
                 arguments = arguments.trim(); // Remove leading and trailing spaces
                 output = arguments;
-            }
-            else {
+            } else {
                 output = "Command '" + command + "' not found";
             }
 
