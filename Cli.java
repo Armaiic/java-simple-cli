6aa55594a2adbd9148fd93c57945954f4e6de65d
Armaiic <bastiendandre.pro@gmail.com>
2023-10-17 16:39:13 +0200

    add_switch_case

--- Cli.java
+++ Cli.java
@@ -1,52 +1,63 @@
-    import java.util.Scanner;
-    import java.time.LocalDate;
-    import java.time.LocalTime;
-    import java.time.LocalDateTime;
+import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.time.LocalDateTime;
 
-    public class Cli {
+public class Cli {
 
-        public static void main(String[] args) {
-            Scanner scanner = new Scanner(System.in);
-            System.out.print("> ");
-            String command;
-            while (!(command = scanner.nextLine()).equals("exit")) {
-                String output = "";
-             String [] commandArg = command.trim().split(" ",2);
-                if (command.equals("date")) {
-                    LocalDate now = LocalDate.now();
-                    output = now.toString();
-                } else if (command.equals("time")) {
-                    LocalTime now = LocalTime.now();
-                    output = now.toString();
-                } else if (command.equals("datetime")) {
-                    LocalDateTime now = LocalDateTime.now();
-                    output = now.toString();
-                } else if (command.equals("useraccount")) {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("> ");
+        String command;
+
+        while (!(command = scanner.nextLine()).equals("exit")) {
+            String output = "";
+            String[] commandArg = command.trim().split(" ", 2);
+
+            switch (commandArg[0]) {
+                case "date":
+                    LocalDate currentDate = LocalDate.now();
+                    output = currentDate.toString();
+                    break;
+                case "time":
+                    LocalTime currentTime = LocalTime.now();
+                    output = currentTime.toString();
+                    break;
+                case "datetime":
+                    LocalDateTime currentDateTime = LocalDateTime.now();
+                    output = currentDateTime.toString();
+                    break;
+                case "useraccount":
                     output = System.getProperty("user.name");
-                } else if (command.equals("userhome")) {
+                    break;
+                case "userhome":
                     output = System.getProperty("user.home");
-                } else if (command.equals("os")) {
+                    break;
+                case "os":
                     output = System.getProperty("os.name") + " (" + System.getProperty("os.version") + ")";
-                
-                }else if (commandArg[0].equals("printenv")){
-                    
-                if (commandArg.length > 1)   {
-                     String envalue = System.getenv(commandArg[1]);      
-                 if (  envalue !=null) {
-                      output = envalue ;
+                    break;
+                case "printenv":
+                    if (commandArg.length > 1) {
+                        String envValue = System.getenv(commandArg[1]);
+                        if (envValue != null) {
+                            output = envValue;
+                        }
                     }
-                }
-           }else if (commandArg[0].equals("echo")) {
-               if (commandArg.length > 1) {
-              output = commandArg[1];
-           }
-           }else {
-                    output = "Command '" + command + "' not found";
-                }
-
-                System.out.println(output);
-                System.out.print("> ");
+                    break;
+                case "echo":
+                    if (commandArg.length > 1) {
+                        output = commandArg[1];
+                    }
+                    break;
+                default:
+                    output = "Command '" + commandArg[0] + "' not found";
+                    break;
             }
-            scanner.close();
+
+            System.out.println(output);
+            System.out.print("> ");
         }
-    }  
+
+        scanner.close();
+    }
+}
